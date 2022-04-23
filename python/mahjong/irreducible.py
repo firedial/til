d57defbaad60a6786acf98af442c3671a5583508
firedial <42674772+firedial@users.noreply.github.com>
2022-04-23 17:03:30 +0900

    雀頭接続順子についての処理を追加

--- python/mahjong/irreducible.py
+++ python/mahjong/irreducible.py
@@ -43,6 +43,21 @@ def isAtamaIrreducible(hai):
     else:
         return True
 
+def isAtamaConnectedShuntsuIrreducible(hai):
+    countWaitingHai = wait.countWaitingHai(hai)
+
+    for hai_ in remove.removedAtamaConnectedShuntsuPattern(hai):
+        count = wait.countWaitingHai(hai_)
+        
+        # 除去した牌形が和了形なら除去した方に和了牌があるので +2 する
+        if agari.isAgari(hai_):
+            count += 2
+
+        if countWaitingHai == count:
+            return False
+    else:
+        return True
+
 def isIrreducible(hai):
     # 両端に架空の牌を付け加える
     h = extendHai(hai)
@@ -52,13 +67,14 @@ def isIrreducible(hai):
 
     # 正規形のときは待ち送り形の既約もみる
     if sum(h) % 3 == 1:
-        if not isAtamaIrreducible(h):
+        if not isAtamaIrreducible(h) or not isAtamaConnectedShuntsuIrreducible(h):
             return False
 
     return True
 
 if __name__ == '__main__':
     print(isMentsuIrreducible([3, 1, 1, 0, 0, 2]))
-    print(isAtamaIrreducible([3, 1, 1]))
+    print(isAtamaIrreducible([3, 1, 1, 0, 0, 1, 1, 3]))
     print(isIrreducible([1, 1, 1, 1, 1]))
+    print(isIrreducible([3, 1, 1, 0, 0, 1, 1, 3]))
 

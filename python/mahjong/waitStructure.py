ad3cb0d02957b983cf41ad01e9802da458676fff
firedial <42674772+firedial@users.noreply.github.com>
2022-05-04 19:03:16 +0900

    わかりやすいように辞書式で管理するようにした

--- python/mahjong/waitStructure.py
+++ python/mahjong/waitStructure.py
@@ -6,15 +6,16 @@ def getWaitingStructure(hai):
     haiLen = len(hai)
     waitingStructure = []
 
-    # 各牌に対して一枚減らして和了かどうか確認していく
+    # 各牌に対して各種待ちとなるかをみていく
     for index in range(0, haiLen):
         # 存在する牌の枚数以上持っていた場合は和了牌とならない
-
-        hai_ = hai[:]
-        if hai_[index] >= MAX_HAI_NUMBER:
-            waitingStructure.append([
-                False, False, False, False
-            ])
+        if hai[index] >= MAX_HAI_NUMBER:
+            waitingStructure.append({
+                'tanki': False,
+                'shanpon': False,
+                'kanchan': False,
+                'ryanmen': False
+            })
             continue
 
 
@@ -38,27 +39,16 @@ def getWaitingStructure(hai):
             kanchan = agari.isAgari(hai_)
 
         # リャンメン
-        if (haiLen <= 3):
-            ryanmenA = False
-            ryanmenB = False
-        elif (index >= haiLen - 3):
-            hai_ = hai[:]
-            hai_[index - 1] -= 1
-            hai_[index - 2] -= 1
-            ryanmenA = agari.isAgari(hai_)
-            ryanmenB = False
-        elif (index <= 2):
-            hai_ = hai[:]
-            hai_[index + 1] -= 1
-            hai_[index + 2] -= 1
-            ryanmenA = False
-            ryanmenB = agari.isAgari(hai_)
-        else:
+        ryanmenA = False
+        ryanmenB = False
+
+        if (index > 2):
             hai_ = hai[:]
             hai_[index - 1] -= 1
             hai_[index - 2] -= 1
             ryanmenA = agari.isAgari(hai_)
 
+        if (index < haiLen - 3):
             hai_ = hai[:]
             hai_[index + 1] -= 1
             hai_[index + 2] -= 1
@@ -66,24 +56,51 @@ def getWaitingStructure(hai):
 
         ryanmen = ryanmenA or ryanmenB
 
-        # waitingStructure.append({
-        #     'tanki': tanki,
-        #     'shanpon': shanpon,
-        #     'kanchan': kanchan,
-        #     'ryanmen': ryanmen
-        # })
+        waitingStructure.append({
+            'tanki': tanki,
+            'shanpon': shanpon,
+            'kanchan': kanchan,
+            'ryanmen': ryanmen
+        })
 
 
-        waitingStructure.append([
-            tanki, shanpon, kanchan, ryanmen
-        ])
+        # waitingStructure.append([
+        #     tanki, shanpon, kanchan, ryanmen
+        # ])
 
     return waitingStructure
 
 def countWaitingStructure(hai):
-    return sum(map(lambda x: len(list(filter(lambda y: y, x))) , getWaitingStructure(hai)))
+    return sum(map(lambda x: len(list(filter(lambda y: y[1], x))) , getWaitingStructure(hai)))
+
+def printWaitingStructure(hai):
+    p = {}
+    p['ryanmen'] = ''
+    p['tanki'] = ''
+    p['shanpon'] = ''
+    p['kanchan'] = ''
+
+    s = getWaitingStructure(hai)
+    for wait in s:
+        for k, v in wait.items():
+            if wait[k]:
+                p[k] += 'o'
+            else:
+                p[k] += '-'
+
+    print(p['ryanmen'])
+    print(p['tanki'])
+    print(p['shanpon'])
+    print(p['kanchan'])
+
+
+
 
 if __name__ == '__main__':
     print(getWaitingStructure([1, 3, 1]))
     print(countWaitingStructure([1, 3, 1]))
+    print(getWaitingStructure([3, 1, 1, 1, 1, 1, 1, 1, 3]))
     print(countWaitingStructure([3, 1, 1, 1, 1, 1, 1, 1, 3]))
+
+    printWaitingStructure([3, 1, 1, 1, 1, 1, 1, 1, 3])
+    printWaitingStructure([3, 1, 1, 1, 2])

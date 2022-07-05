a66d60e3c275f34af38aad0e780e989217ddcfc2
firedial <42674772+firedial@users.noreply.github.com>
2022-07-05 20:28:20 +0900

    コメント入れたりした

--- python/mahjong/wait.py
+++ python/mahjong/wait.py
@@ -2,29 +2,47 @@ import agari
 
 MAX_HAI_NUMBER = 4
 
-def getWaitingHai(hai):
-    haiLen = len(hai)
-    waitingHai = []
+def getWaitingHai(hand: list[int]) -> list[bool]:
+    """
+    待ち牌のリストを求める
+
+    Args:
+        hand (list[int]): 牌形
+
+    Returns:
+        list[bool]: 和了牌なら True / それ以外 False のリスト
+    """
+    handLength: int = len(hand)
+    waitingHai: list[bool] = []
 
     # 各牌に対して一枚ずつ増やして和了かどうか確認していく
-    for index in range(0, haiLen):
+    for index in range(0, handLength):
         # 存在する牌の枚数以上持っていた場合は和了牌とならない
-        if hai[index] >= MAX_HAI_NUMBER:
+        if hand[index] >= MAX_HAI_NUMBER:
             waitingHai.append(False)
 
-        hai_ = hai[:]
-        hai_[index] += 1
-        waitingHai.append(agari.isAgari(hai_))
+        hand_ = hand[:]
+        hand_[index] += 1
+        waitingHai.append(agari.isAgari(hand_))
 
     return waitingHai
 
-def countWaitingHai(hai):
-    return len(list(filter(lambda x: x, getWaitingHai(hai))))
-    
-    
+def countWaitingHai(hand: list[int]) -> int:
+    """
+    待ち牌の種類数を求める
+
+    Args:
+        hand (list[int]): 牌形
+
+    Returns:
+        int: 待ち牌の種類数
+    """
+    return len(list(filter(lambda x: x, getWaitingHai(hand))))
+
+
 if __name__ == '__main__':
-    print(getWaitingHai([3, 1, 1, 1, 1, 1, 1, 1, 3]))
-    print(countWaitingHai([1, 1, 1, 1, 1, 1, 1]))
-    print(countWaitingHai([3, 1, 1, 1, 1, 1, 1, 1, 3]))
+    assert getWaitingHai([3, 1, 1, 1, 1, 1, 1, 1, 3]) == [True, True, True, True, True, True, True, True, True]
+    assert countWaitingHai([1, 1, 1, 1, 1, 1, 1]) == 3
+    assert countWaitingHai([3, 1, 1, 1, 1, 1, 1, 1, 3]) == 9
 
 

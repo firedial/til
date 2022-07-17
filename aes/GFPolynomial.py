7e466a93434d6a94a574a46f3e0d75cd6be723fc
firedial <42674772+firedial@users.noreply.github.com>
2022-07-17 21:31:16 +0900

    色々バグがあったので修正

--- aes/GFPolynomial.py
+++ aes/GFPolynomial.py
@@ -1,32 +1,36 @@
+from msilib.schema import Billboard
+from typing import List
 
 class GFPolynomial:
 
     modulo = 0x11b
 
     def __init__(self, cofficient: int):
-        self.cofficient = cofficient
+        overBitCount = cofficient.bit_length() - self.modulo.bit_length()
 
+        cofficientShift = cofficient.bit_length() - 1
+        
+        while overBitCount >= 0:
+            if cofficient >> cofficientShift & 1 == 1:
+                cofficient ^= self.modulo << overBitCount
+            overBitCount -= 1
+            cofficientShift -= 1
+        
+        self.cofficient = cofficient
+        assert(cofficient.bit_length() < self.modulo.bit_length())
+        
     def __add__(self, rhs):
         return GFPolynomial(self.cofficient ^ rhs.cofficient)
     
     def __mul__(self, rhs):
-        r = 0
-        lhs = self.cofficient
-        
-        while True:
-            if lhs == 0:
-                break
+        m = 0
 
-            r <<= 1
-            if r.bit_length() == self.modulo.bit_length():
-                r ^= self.modulo
+        for i in range(self.cofficient.bit_length()):
+            for j in range(rhs.cofficient.bit_length()):
+                if (self.cofficient >> i & 1 == 1) and (rhs.cofficient >> j & 1 == 1):
+                    m ^= (1 << (i + j))
 
-            if lhs & 1 == 1:
-                r ^= rhs.cofficient
-            
-            lhs >>= 1
-        return GFPolynomial(r)
-
+        return GFPolynomial(m) 
     
     def pprint(self) -> str:
         poly = []
@@ -40,16 +44,33 @@ class GFPolynomial:
         return self.cofficient
 
 
+__inverse: List[GFPolynomial] = [GFPolynomial(0)] * 256
+for i in range(256):
+    for j in range(256):
+        m = GFPolynomial(i) * GFPolynomial(j)
+        if m.getCofficient() == 1:
+            __inverse[i]= GFPolynomial(j)
+            break
+
+
+print(list(map(lambda x: x.getCofficient(), __inverse)))
+
+def inverse(poly) -> GFPolynomial:
+    return __inverse[poly.getCofficient()]
+
 
 if __name__ == "__main__":
     a = GFPolynomial(0b11011)
     b = GFPolynomial(0b10111)
     c = a + b
     d = a * b
-    assert(c.getCofficient() == 0b1100)
-    assert(d.getCofficient() == 0b11101010)
 
-    print(a.pprint())
-    print(b.pprint())
-    print(c.pprint())
-    print(d.pprint())
+    assert(c.cofficient == 0b1100)
+    assert(d.cofficient == 0b11101010)
+
+    r = inverse(a)
+    assert(r.cofficient == 0b11001100)
+
+    e = r * a
+    assert(e.cofficient == 1)
+

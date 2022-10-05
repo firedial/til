68a4d39bddee0ed2a392cfe7b0c770ee162341e8
firedial <42674772+firedial@users.noreply.github.com>
2022-10-05 23:59:26 +0900

    表示についてもろもろ調整

--- python/mahjong/main.py
+++ python/mahjong/main.py
@@ -1,4 +1,3 @@
-
 import sys
 
 import agari
@@ -6,30 +5,60 @@ import handLoop
 import form
 import shanten
 import irreducible
+import wait
 
 # print(agari.isAgari([3, 1, 1, 1, 1, 1, 1, 1, 4]))
 # print(agari.isSevenPairs([2, 2, 2, 2, 2, 2, 2, 2]))
 
 
 count = 0
-num = int(sys.argv[1])
-
-hai = [0, 0, 0, 0, 0, 0, 0, 0, num]
+# num = inut(sys.argv[1])
+num = 8
 
 hai = [0, 0, 0, 0, 0, 0, 0, 0, num]
 while True:
     # if (not form.isOverFour(hai)) and shanten.isTempai(hai):
-    if (not form.isOverFour(hai)
-            and form.isBasicForm(hai)
-            and shanten.isTempai(hai)
-            # and irreducibleStructure.isIrreducible(hai)):
-            and irreducible.isIrreducible(hai)):
-        print(hai)
+    if (
+        not form.isOverFour(hai)
+        and form.isBasicForm(hai)
+        and shanten.isTempai(hai)
+        # and irreducibleStructure.isIrreducible(hai)):
+        and irreducible.isIrreducible(hai)
+    ):
+        waitingList = wait.getWaitingHai(hai)
+        waitingKindCount = len(list(filter(lambda x: x, waitingList)))
+        waitingCount = 0
+        for (h, w) in zip(hai, waitingList):
+            if w:
+                waitingCount += 4 - h
+
+        # 両側の0を省く
+        bothAttachHai = form.getUniformForm(hai)
+        if len(bothAttachHai) <= 7:
+            leftIrreducible = irreducible.isIrreducible(bothAttachHai + [0])
+            rightIrreducible = irreducible.isIrreducible([0] + bothAttachHai)
+            print("o" if leftIrreducible else "x", end="")
+            print("o" if rightIrreducible else "x", end="|")
+        elif len(bothAttachHai) == 8:
+            if hai[0] != 0:
+                rightIrreducible = irreducible.isIrreducible([0] + bothAttachHai)
+                print("-", end="")
+                print("o" if rightIrreducible else "x", end="|")
+            else:
+                leftIrreducible = irreducible.isIrreducible(bothAttachHai + [0])
+                print("o" if leftIrreducible else "x", end="")
+                print("-", end="|")
+
+        elif len(bothAttachHai) == 9:
+            print("--", end="|")
+
+        print(hai, end="||")
+        print(list(map(lambda x: 1 if x else 0, waitingList)), end="||")
+        print(waitingKindCount, end="|")
+        print(waitingCount)
         count += 1
     hai = handLoop.nextHand(hai)
-    if (hai == [0, 0, 0, 0, 0, 0, 0, 0, num]):
+    if hai == [0, 0, 0, 0, 0, 0, 0, 0, num]:
         break
 
 print(count)
-
-

3ea18a13d69a65586f1f4176c7e7324f36dfb477
firedial <42674772+firedial@users.noreply.github.com>
2022-10-08 00:26:16 +0900

    処理をクラスの方に移した

--- python/mahjong/main.py
+++ python/mahjong/main.py
@@ -1,74 +1,32 @@
-from re import I
-import sys
+from HandUtil import HandUtil
 
-import agari
 import handLoop
-import form
-import shanten
-import irreducible
-import wait
-
-# print(agari.isAgari([3, 1, 1, 1, 1, 1, 1, 1, 4]))
-# print(agari.isSevenPairs([2, 2, 2, 2, 2, 2, 2, 2]))
 
 
-count = 0
-# num = inut(sys.argv[1])
-num = 11
+def loopHand(fp, num: int):
+    handUtil = HandUtil()
+    hand = [0, 0, 0, 0, 0, 0, 0, 0, num]
+    while True:
+        handUtil.setHand(hand)
+        if handUtil.isNeedIrreducible():
+            fp.write(handUtil.printHandDetail())
+            fp.write("\n")
 
-hai = [0, 0, 0, 0, 0, 0, 0, 0, num]
-while True:
-    # if (not form.isOverFour(hai)) and shanten.isTempai(hai):
-    if (
-        not form.isOverFour(hai)
-        and form.isBasicForm(hai)
-        and shanten.isTempai(hai)
-        # and irreducibleStructure.isIrreducible(hai)):
-        and irreducible.isIrreducible(hai)
-    ):
-        haiCount = sum(hai)
-        print(haiCount, end="|")
+            # 雀頭接続順子も考える
+            if handUtil.hasAtamaConnectedShuntsu():
+                handUtil.setHand(hand, True)
+                fp.write(handUtil.printHandDetail())
+                fp.write("\n")
 
-        waitingList = wait.getWaitingHai(hai)
-        waitingKindCount = len(list(filter(lambda x: x, waitingList)))
-        waitingCount = 0
-        for (h, w) in zip(hai, waitingList):
-            if w:
-                waitingCount += 4 - h
+        hand = handLoop.nextHand(hand)
+        if hand == [0, 0, 0, 0, 0, 0, 0, 0, num]:
+            break
 
-        # 両側の0を省く
-        bothAttachHai = form.getUniformForm(hai)
-        if len(bothAttachHai) <= 7:
-            leftIrreducible = shanten.isTempai(bothAttachHai + [0]) and irreducible.isIrreducible(bothAttachHai + [0])
-            rightIrreducible = shanten.isTempai([0] + bothAttachHai) and irreducible.isIrreducible([0] + bothAttachHai)
-            print("o" if leftIrreducible else "x", end="")
-            print("o" if rightIrreducible else "x", end="|")
-        elif len(bothAttachHai) == 8:
-            if hai[0] != 0:
-                rightIrreducible = shanten.isTempai([0] + bothAttachHai) and irreducible.isIrreducible([0] + bothAttachHai)
-                print("-", end="")
-                print("o" if rightIrreducible else "x", end="|")
-            else:
-                leftIrreducible = shanten.isTempai(bothAttachHai + [0]) and irreducible.isIrreducible(bothAttachHai + [0])
-                print("o" if leftIrreducible else "x", end="")
-                print("-", end="|")
 
-        elif len(bothAttachHai) == 9:
-            print("--", end="|")
+numbers = [1, 2, 4, 5, 7, 8, 10, 11, 13]
+fp = open("list.txt", "w")
 
-        print(hai, end="||")
-        print(list(map(lambda x: 1 if x else 0, waitingList)), end="||")
+for n in numbers:
+    loopHand(fp, n)
 
-        if haiCount % 3 == 2 and agari.isAgari(hai):
-            print("m", end="|")
-        else:
-            print("-", end="|")
-
-        print(waitingKindCount, end="|")
-        print(waitingCount)
-        count += 1
-    hai = handLoop.nextHand(hai)
-    if hai == [0, 0, 0, 0, 0, 0, 0, 0, num]:
-        break
-
-print(count)
+fp.close()

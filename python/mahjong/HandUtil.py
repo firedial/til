3a7ad49dae1a98086a09c46e17670197685eacd0
firedial <42674772+firedial@users.noreply.github.com>
2022-10-11 16:33:02 +0900

    牌番号でセットできるようにした

--- python/mahjong/HandUtil.py
+++ python/mahjong/HandUtil.py
@@ -1,5 +1,3 @@
-from re import I
-from turtle import left
 import agari
 import shanten
 import form
@@ -14,6 +12,24 @@ class HandUtil:
     def setHand(self, hand: list[int], isConnected: bool = False) -> None:
         self.hand = hand
         self.isConnected = isConnected
+
+    def setHandByNumber(self, number: int) -> None:
+        # 5 進数の数字に変換する
+        n = []
+        while number != 0:
+            n.append(number % 5)
+            number //= 5
+        n.reverse()
+
+        # 左接地させるかどうか判定
+        if n[-1] != 3 and n[-1] != 1:
+            # 末尾が 3 でない(長さが 9 でない) または末尾が 1 でない(長さ 8 の左接地でない)
+            self.hand = list(map(lambda x: 4 - x, [4] + n[2:10]))
+        else:
+            self.hand = list(map(lambda x: 4 - x, n[2:11]))
+
+        # 雀頭接続順子
+        self.isConnected = n[1] == 0
 
     def isValid(self) -> bool:
         # 0 枚未満の牌を持っているとき
@@ -52,14 +68,14 @@ class HandUtil:
 
     def getHandNumber(self) -> int:
         handCount = sum(self.hand)
-        handCountMap = {1: 1, 4: 2, 7: 3, 10: 4, 13: 5}
+        handCountMap = {1: 0, 4: 1, 7: 2, 10: 3, 13: 4}
 
+        leftAttachHand = self.getLeftAttachHand()
         normalHandNumber = 2 if handCount % 3 != 2 else (0 if self.isConnected else 1)
         normalHandCount = handCount + (2 if handCount % 3 == 2 else 0) + (3 if self.isConnected else 0)
-        place = 0 if len(self.hand) <= 7 else (1 if self.hand[0] != 0 else 2)
+        place = 0 if leftAttachHand[-1] == 0 and leftAttachHand[-2] == 0 else (3 if leftAttachHand[-1] != 0 else (1 if self.hand[0] != 0 else 2))
 
         number = 0
-        leftAttachHand = self.getLeftAttachHand()
 
         handNumber = [handCountMap[normalHandCount]] + [normalHandNumber] + list(map(lambda x: 4 - x, leftAttachHand)) + [place]
 
@@ -138,3 +154,28 @@ if __name__ == "__main__":
 
     h.setHand([2, -1, 4])
     assert not h.isValid()
+
+    numbers = []
+    # 通常
+    h.setHand([0, 1, 1, 1, 1, 0, 0, 0, 0])
+    numbers.append(h.getHandNumber())
+
+    # 雀頭関係
+    h.setHand([0, 3, 1, 1, 0, 0, 0, 0, 0])
+    numbers.append(h.getHandNumber())
+    h.setHand([0, 3, 1, 1, 0, 0, 0, 0, 0], True)
+    numbers.append(h.getHandNumber())
+
+    # 長さ8関係
+    h.setHand([3, 0, 1, 1, 1, 1, 0, 3, 0])
+    numbers.append(h.getHandNumber())
+    h.setHand([0, 3, 0, 1, 1, 1, 1, 0, 3])
+    numbers.append(h.getHandNumber())
+
+    # 長さ9
+    h.setHand([3, 1, 1, 1, 1, 1, 1, 1, 3])
+    numbers.append(h.getHandNumber())
+
+    for n in numbers:
+        h.setHandByNumber(n)
+        assert n == h.getHandNumber()

9ad4fc1c2dd4e6a34b8ba0ec1d2f79c78650275f
firedial <42674772+firedial@users.noreply.github.com>
2022-10-11 17:00:07 +0900

    雀頭接続順子についてミスっていた

--- python/mahjong/main.py
+++ python/mahjong/main.py
@@ -3,18 +3,20 @@ from HandUtil import HandUtil
 import handLoop
 
 
-def loopHand(hands, num: int):
+def loopHand(fp, num: int):
     handUtil = HandUtil()
     hand = [0, 0, 0, 0, 0, 0, 0, 0, num]
     while True:
         handUtil.setHand(hand)
         if handUtil.isNeedIrreducible():
-            hands[handUtil.getHandNumber()] = handUtil.printHandDetail()
+            fp.write(str(handUtil.getHandNumber()))
+            fp.write("\n")
 
             # 雀頭接続順子も考える
             if handUtil.hasAtamaConnectedShuntsu():
                 handUtil.setHand(hand, True)
-                hands[handUtil.getHandNumber()] = handUtil.printHandDetail()
+                fp.write(str(handUtil.getHandNumber()))
+                fp.write("\n")
 
         hand = handLoop.nextHand(hand)
         if hand == [0, 0, 0, 0, 0, 0, 0, 0, num]:
@@ -22,37 +24,9 @@ def loopHand(hands, num: int):
 
 
 numbers = [1, 2, 4, 5, 7, 8, 10, 11, 13]
-# numbers = [1, 2, 4, 5, 7]
-hands = {}
-
-fp = open("list.txt", "w")
+fp = open("hands.txt", "w")
 
 for n in numbers:
-    loopHand(hands, n)
-
-hands = dict(sorted(hands.items()))
-waitingNumber = 1
-beforeNumber = 0
-for h in hands:
-    wait = "W" + str(waitingNumber).zfill(3)
-    currentNumber = h // 5
-
-    # 左接地
-    if h % 5 == 1:
-        wait += "-l"
-    # 右接地
-    elif h % 5 == 2:
-        if beforeNumber == currentNumber:
-            waitingNumber -= 1
-        wait += "-r"
-    else:
-        wait += "  "
-
-    waitingNumber += 1
-
-    fp.write(wait + hands[h])
-    fp.write("\n")
-
-    beforeNumber = currentNumber
+    loopHand(fp, n)
 
 fp.close()

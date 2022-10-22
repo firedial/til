b7519f918baf8f73d4bde7d089e55957ae6f5155
firedial <42674772+firedial@users.noreply.github.com>
2022-10-23 01:05:54 +0900

    アルゴリズムの変更

--- polyomino/main.py
+++ polyomino/main.py
@@ -18,7 +18,7 @@ def deletePolyomino(f, p, x, y, d):
         f[x + dx][y + dy] = EMPTY_CELL
 
 
-def putPolyomino(f, n, p, pn, x, y, d):
+def putPolyomino(f, n, p, pn, x, y, d) -> bool:
     """
     d = 0
         そのまま
@@ -35,12 +35,12 @@ def putPolyomino(f, n, p, pn, x, y, d):
             dx, dy = dy, -dx
 
         if not (0 <= (x + dx) < n):
-            return None
+            return False
         if not (0 <= (y + dy) < n):
-            return None
+            return False
 
         if f[x + dx][y + dy] != EMPTY_CELL:
-            return None
+            return False
 
     for dx, dy in p:
         if d == 1:
@@ -55,57 +55,66 @@ def putPolyomino(f, n, p, pn, x, y, d):
     return True
 
 
-def isFull(field):
-    for rows in field:
-        for cell in rows:
-            if cell == EMPTY_CELL:
-                return False
+def countFullPolyominoSet(field, n, polyominos) -> int:
+    if len(polyominos) == 0:
+        return 1
 
-    return True
-
-
-def main(polyomino, n, f, pn, result):
-    if len(polyomino) == 0:
-        return
-
-    p = polyomino.pop()
-
+    count = 0
+    polyomino = polyominos.pop()
     for i in range(n):
         for j in range(n):
             if f[i][j] != EMPTY_CELL:
                 continue
+
             for d in range(4):
-                if putPolyomino(f, n, p, pn, i, j, d) is None:
+                if not putPolyomino(field, n, polyomino["form"], polyomino["id"], i, j, d):
                     continue
 
-                if isFull(f):
-                    key = frozenset((itertools.chain.from_iterable(f)))
-                    result[key] = result.get(key, 0) + 1
-                    deletePolyomino(f, p, i, j, d)
-                    continue
+                count += countFullPolyominoSet(field, n, polyominos)
+                deletePolyomino(field, polyomino["form"], i, j, d)
 
-                main(polyomino, n, f, pn + 1, result)
-                deletePolyomino(f, p, i, j, d)
+    polyominos.append(polyomino)
+    return count
 
-    main(polyomino, n, f, pn + 1, result)
-    polyomino.append(p)
+
+def main(polyominos, polyominoSet, n, result):
+    s = sum(map(lambda x: x["count"], polyominoSet))
+    if n * n == s:
+        field = [[EMPTY_CELL for _ in range(n)] for _ in range(n)]
+        count = countFullPolyominoSet(field, n, polyominoSet)
+        if count != 0:
+            uniqueCount = functools.reduce(operator.floordiv, map(lambda x: 4 // x["rotate"], polyominoSet), count)
+            result[frozenset(map(lambda x: x["id"], polyominoSet))] = uniqueCount // 4
+
+        return
+
+    if n * n < s or len(polyominos) == 0:
+        return
+
+    p = polyominos.pop()
+    polyominoSet.append(p)
+    main(polyominos, polyominoSet, n, result)
+
+    polyominoSet.pop()
+    main(polyominos, polyominoSet, n, result)
+    polyominos.append(p)
 
 
 n = 3
-result = {}
 f = [[EMPTY_CELL for _ in range(n)] for _ in range(n)]
+polyominos = polyominoList.getPolyominos()
 
-polyomino = polyominoList.getPolyomino()
-polyominoRoutate = polyominoList.getPolyominoRotate()
+r = countFullPolyominoSet(f, n, [polyominos[0], polyominos[6], polyominos[10]])
 
-main(polyomino, n, f, 0, result)
-
-answer = []
-polyominoRoutate.reverse()
-for k, v in result.items():
-    count = functools.reduce(operator.floordiv, map(lambda y: 4 // polyominoRoutate[y], k), v)
-    if count == 4:
-        answer.append(k)
-
+result = {}
+main(polyominos, [], n, result)
 print(result)
-print(answer)
+
+# answer = []
+# for k, v in result.items():
+#     count = functools.reduce(operator.floordiv, map(lambda y: 4 // polyominoRoutate[y], k), v)
+#     if count == 4:
+#         answer.append(k)
+#
+# print(result)
+# print(answer)

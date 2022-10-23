4c98e26ac817f77750c8813d280307d952320f7b
firedial <42674772+firedial@users.noreply.github.com>
2022-10-23 20:40:41 +0900

    正解のパターンも出せるようにした

--- polyomino/main.py
+++ polyomino/main.py
@@ -1,7 +1,8 @@
-import itertools
 import functools
 import operator
 import polyominoList
+import random
+import copy
 
 EMPTY_CELL = -1
 
@@ -77,11 +78,35 @@ def countFullPolyominoSet(field, n, polyominos) -> int:
     return count
 
 
+def getFullPolyominoSet(field, n, polyominos, answer):
+    if len(polyominos) == 0:
+        answer.append(copy.deepcopy(field))
+        return True
+
+    polyomino = polyominos.pop()
+    for i in range(n):
+        for j in range(n):
+            if f[i][j] != EMPTY_CELL:
+                continue
+
+            for d in range(4):
+                if not putPolyomino(field, n, polyomino["form"], polyomino["id"], i, j, d):
+                    continue
+
+                getFullPolyominoSet(field, n, polyominos, answer)
+                deletePolyomino(field, polyomino["form"], i, j, d)
+
+    polyominos.append(polyomino)
+    return False
+
+
 def main(polyominos, polyominoSet, n, result):
     s = sum(map(lambda x: x["count"], polyominoSet))
     if n * n == s:
+        if random.randrange(1000) >= 20000:
+            return
         field = [[EMPTY_CELL for _ in range(n)] for _ in range(n)]
-        count = countFullPolyominoSet(field, n, polyominoSet)
+        count = countFullPolyominoSet(field, n, polyominoSet.copy())
         if count != 0:
             uniqueCount = functools.reduce(operator.floordiv, map(lambda x: 4 // x["rotate"], polyominoSet), count)
             result[frozenset(map(lambda x: x["id"], polyominoSet))] = uniqueCount // 4
@@ -100,15 +125,41 @@ def main(polyominos, polyominoSet, n, result):
     polyominos.append(p)
 
 
-n = 3
+n = 4
 f = [[EMPTY_CELL for _ in range(n)] for _ in range(n)]
 polyominos = polyominoList.getPolyominos()
 
-r = countFullPolyominoSet(f, n, [polyominos[0], polyominos[6], polyominos[10]])
+# r = countFullPolyominoSet(f, n, [polyominos[0], polyominos[6], polyominos[10]])
 
 result = {}
-main(polyominos, [], n, result)
-print(result)
+usePolyominos = list(filter(lambda x: x["rotate"] == 4, polyominoList.getPolyominos()))
+main(usePolyominos, [], n, result)
+minResult = list(filter(lambda x: x[1] == 2, result.items()))
+
+pieceCountMax = 0
+for r in minResult:
+    f = [[EMPTY_CELL for _ in range(n)] for _ in range(n)]
+    answer = []
+    getFullPolyominoSet(f, n, list(map(lambda x: polyominos[x - 1], r[0])), answer)
+    pieceCount = len(r[0])
+    pieceCountMax = pieceCount if pieceCountMax < pieceCount else pieceCountMax
+
+    print("-" * 50)
+    print("[%d] pattern %d" % (pieceCount, r[1]))
+
+    for a in answer:
+        if max(a[0][0], a[0][-1], a[-1][0], a[-1][-1]) != a[0][0] or max(a[0][-1], a[-1][0], a[-1][-1]) != a[0][-1]:
+            continue
+
+        print("-" * 10)
+        for row in a:
+            for cell in row:
+                print("%3d" % cell, end="")
+            print()
+
+print("-" * 50)
+print(pieceCountMax)
+
 
 # answer = []
 # for k, v in result.items():

d9f7c8668219d1870a99bb672f22e82d92a309ab
firedial <42674772+firedial@users.noreply.github.com>
2024-02-18 20:16:11 +0900

    繰り返し実行の実装

--- startups/play.py
+++ startups/play.py
@@ -1,101 +1,49 @@
-from game import Game
+from game import RoundGame
 import concurrent.futures
 import random
 
-def playerDA(choices):
-    return random.randint(0, len(choices) - 1)
+def roundPlay(playerChoiceFunctions):
+    game = RoundGame(playerChoiceFunctions)
+    return game.play()
 
-def playerDB(choices):
-    return 0
+def playerA(status):
+    choices = status["choices"]
+    if status["phase"] == "draw":
+        return random.randint(0, len(choices) - 1)
+    else:
+        return random.randint(0, len(choices) - 1)
 
-def playerDC(choices):
-    return 0
-    return random.randint(0, len(choices) - 1)
-    return len(choices) - 1
+def playerB(status):
+    choices = status["choices"]
+    if status["phase"] == "draw":
+        return random.randint(0, len(choices) - 1)
+    else:
+        return random.randint(0, len(choices) - 1)
 
-def playerAA(choices):
-    return random.randint(0, len(choices) - 1)
+def playerC(status):
+    choices = status["choices"]
+    if status["phase"] == "draw":
+        return random.randint(0, len(choices) - 1)
+    else:
+        return random.randint(0, len(choices) - 1)
 
-def playerAB(choices):
-    return len(choices) - 1
-    return random.randint(0, len(choices) - 1)
+playerChoiceFunctions = [
+    playerA,
+    playerB,
+    playerC,
+]
+playerCount = len(playerChoiceFunctions)
+results = [[0 for _ in range(playerCount)] for _ in range(playerCount)]
+count = 10000
 
-def playerAC(choices):
-    return len(choices) - 1
-    return random.randint(0, len(choices) - 1)
-
-def play(players):
-    game = Game(3)
-    count = 0
-
-    while game.hasDeck():
-        choices = game.getChoices()
-        match players[count % 3]:
-            case 0:
-                choiceIndex = playerDA(choices)
-            case 1:
-                choiceIndex = playerDB(choices)
-            case 2:
-                choiceIndex = playerDC(choices)
-        game.inputChoiceIndex(choiceIndex)
-
-        choices = game.getChoices()
-        match players[count % 3]:
-            case 0:
-                choiceIndex = playerAA(choices)
-            case 1:
-                choiceIndex = playerAB(choices)
-            case 2:
-                choiceIndex = playerAC(choices)
-        game.inputChoiceIndex(choiceIndex)
-
-        count += 1
-
-    gameResult = game.getResult()
-    result = [0, 0, 0]
-    result[players[0]] = gameResult[0]
-    result[players[1]] = gameResult[1]
-    result[players[2]] = gameResult[2]
-
-    return result
-
-def roundPlay():
-    point = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-    players = [0, 1, 2]
-    random.shuffle(players)
-
-    for _ in range(4):
-        result = play(players)
-        minPlayerIndex = -1
-        for index, value in enumerate(result):
-            match value:
-                case 2:
-                    point[index][0] += 1
-                case 1:
-                    point[index][1] += 1
-                case -1:
-                    point[index][2] += 1
-                    minPlayerIndex = index
-
-        # 最下位のプレイヤーを最初にする
-        while True:
-            if players[0] == minPlayerIndex:
-                break
-
-            players.append(players.pop(0))
-
-    return result
-
-
-result = [0, 0, 0]
 with concurrent.futures.ProcessPoolExecutor() as executor:
-    futures = [executor.submit(roundPlay) for _ in range(5)]
+    futures = [executor.submit(roundPlay, playerChoiceFunctions) for _ in range(count)]
 
     for future in concurrent.futures.as_completed(futures):
         gameResult = future.result()
-        result[0] += gameResult[0]
-        result[1] += gameResult[1]
-        result[2] += gameResult[2]
+        for index, rank in enumerate(gameResult):
+            results[index][rank - 1] += 1
 
-print(result)
+print(results)
+for result in results:
+    print(result[0] / count)

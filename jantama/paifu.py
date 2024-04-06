2586c362ba9e4f551fd3c25f5f977862e32b9258
firedial <42674772+firedial@users.noreply.github.com>
2024-04-06 11:20:42 +0900

    局終了の判定追加

--- jantama/paifu.py
+++ jantama/paifu.py
@@ -10,3 +10,23 @@ class Paifu:
     def allTime(self) -> int:
         return self.paifu["head"]["end_time"] - self.paifu["head"]["start_time"]
 
+    @staticmethod
+    def isFinishRound(action: dict) -> bool:
+        # 牌譜のタイプが1の時に記録されている(そして result のキーを持つ)
+        if action["type"] != 1:
+            return False
+
+        # 荒牌平局
+        if action["result"]["name"] == ".lq.RecordNoTile":
+            return True
+
+        # 途中流局
+        if action["result"]["name"] == ".lq.RecordLiuJu":
+            return True
+
+        # 和了
+        if action["result"]["name"] == ".lq.RecordHule":
+            return True
+
+        return False
+

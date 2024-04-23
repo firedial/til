2b855a77bdd4caa0359345557fc581729ef48b81
firedial <42674772+firedial@users.noreply.github.com>
2024-04-23 22:28:41 +0900

    wip

--- jantama/main.py
+++ jantama/main.py
@@ -5,5 +5,5 @@ f = open('paifu/mahjongsoul_paifu_220508-33d67351-5ec4-4799-9a26-a78c1da05b4f.tx
 p = Paifu(json.load(f))
 f.close()
 
-print(p.allTime())
+print(p.debug())
 
--- jantama/paifu.py
+++ jantama/paifu.py
@@ -1,17 +1,57 @@
 from dataclasses import dataclass
+from mahjong.shanten import Shanten
+from mahjong.tile import TilesConverter
+
+@dataclass
+class Tile:
+    tiles: list[str]
+
+    def __init__(self, tiles: dict):
+        self.tiles = tiles
+
+    def action(self, action: dict):
+        pass
+
+    def getShanten(self) -> int:
+        m = "".join(list(map(lambda x: x[0], (filter(lambda x: x[-1] == "m", self.tiles)))))
+        p = "".join(list(map(lambda x: x[0], (filter(lambda x: x[-1] == "p", self.tiles)))))
+        s = "".join(list(map(lambda x: x[0], (filter(lambda x: x[-1] == "s", self.tiles)))))
+        h = "".join(list(map(lambda x: x[0], (filter(lambda x: x[-1] == "z", self.tiles)))))
+        shanten = Shanten()
+        tiles = TilesConverter.string_to_34_array(man=m, pin=p, sou=s, honors=h)
+
+        return shanten.calculate_shanten(tiles)
 
 @dataclass
 class Round:
-    tiles0: list[str]
-    tiles1: list[str]
-    tiles2: list[str]
-    tiles3: list[str]
+    tiles0: Tile
+    tiles1: Tile
+    tiles2: Tile
+    tiles3: Tile
 
     def __init__(self, action: dict):
-        self.tiles0 = action["result"]["data"]["tiles0"]
-        self.tiles1 = action["result"]["data"]["tiles1"]
-        self.tiles2 = action["result"]["data"]["tiles2"]
-        self.tiles3 = action["result"]["data"]["tiles3"]
+        self.tiles0 = Tile(action["result"]["data"]["tiles0"])
+        self.tiles1 = Tile(action["result"]["data"]["tiles1"])
+        self.tiles2 = Tile(action["result"]["data"]["tiles2"])
+        self.tiles3 = Tile(action["result"]["data"]["tiles3"])
+
+        print(self.tiles0.getShanten())
+
+    def execute(self, action: dict):
+        return
+        # 牌譜のタイプが1の時に記録されている(そして result のキーを持つ)
+        if action["type"] != 1:
+            return False
+
+        match action["result"]["name"]:
+            case ".lq.RecordDealTile": # 自摸
+                pass
+            case ".lq.RecordDiscardTile": # 打牌
+                pass
+            case ".lq.RecordChiPengGang": # チー、ポン、大明槓
+                pass
+            case ".lq.RecordAnGangAddGang": # 暗槓、加槓
+                pass
 
 @dataclass
 class Paifu:
@@ -47,3 +87,11 @@ class Paifu:
     def isNewRound(action: dict) -> bool:
         return action["type"] == 1 and action["result"]["name"] == ".lq.RecordNewRound"
 
+    def debug(self) -> str:
+        actions = self.paifu["data"]["data"]["actions"]
+        for action in actions:
+            if self.isNewRound(action):
+                round = Round(action)
+            else:
+                pass
+                # round.execute(action)

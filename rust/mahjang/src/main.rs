dd44456b6d1c5156ca3c87b497217bce97a8880c
firedial <42674772+firedial@users.noreply.github.com>
2026-05-05 18:25:13 +0900

    関数に切り出し

--- rust/mahjang/src/main.rs
+++ rust/mahjang/src/main.rs
@@ -1,40 +1,45 @@
 pub mod basic;
 
+fn is_irreducible(suit: &basic::suit::Suit) -> bool {
+    if suit.is_valid_suit() && suit.is_basic() && suit.waiting().is_tempai() {
+        if suit.length() == 9 {
+            if suit.is_irreducible() {
+                return true;
+            }
+        } else if suit.length() == 8 {
+            if suit.is_irreducible() || suit.one_right_slide().is_irreducible() {
+                return true;
+            }
+        } else {
+            if suit.one_right_slide().is_irreducible() {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+
 fn main() {
-    // let suit = basic::suit::Suit {
-    //     suit: [3, 2, 0, 0, 0, 0, 0, 0, 0],
-    // };
+    let suit = basic::suit::Suit {
+        suit: [0, 1, 4, 2, 1, 1, 3, 1, 0],
+    };
     // println!("is agari {}", suit.is_agari());
     // println!("is tempai {}", suit.waiting().is_tempai());
     // println!("waiting {:?}", suit.waiting());
 
-    let mut suit2 = basic::suit::first_suit(13);
-    let mut count = 0;
-    loop {
-        suit2 = suit2.next_suit();
-        if suit2.is_valid_suit() && suit2.is_basic() && suit2.waiting().is_tempai() {
-            // println!("target {:?}", suit2.suit);
-            if suit2.length() == 9 {
-                if suit2.is_irreducible() {
-                    println!("suit {:?}", suit2.suit);
-                    count += 1;
-                }
-            } else if suit2.length() == 8 {
-                if suit2.is_irreducible() || suit2.one_right_slide().is_irreducible() {
-                    println!("suit {:?}", suit2.suit);
-                    count += 1;
-                }
-            } else {
-                if suit2.one_right_slide().is_irreducible() {
-                    println!("suit {:?}", suit2.suit);
-                    count += 1;
-                }
-            }
-        }
+    let r = is_irreducible(&suit);
+    println!("irreducible {:?}", r);
 
-        if suit2.is_first_suit() {
-            break;
-        };
-    }
-    println!("count {}", count);
+    // let mut suit2 = basic::suit::first_suit(13);
+    // let mut count = 0;
+    // loop {
+    //     suit2 = suit2.next_suit();
+
+    //     if suit2.is_first_suit() {
+    //         break;
+    //     };
+    // }
+    // println!("count {}", count);
 }
